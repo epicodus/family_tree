417850a254abe23aae36175bcd74f2486a76f9ab
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2014-03-27 18:01:20 -0700

    build out basic user interface

--- family_tree.rb
+++ family_tree.rb
@@ -5,3 +5,66 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 database_configurations = YAML::load(File.open('./db/config.yml'))
 development_configuration = database_configurations['development']
 ActiveRecord::Base.establish_connection(development_configuration)
+
+def menu
+  puts 'Welcome to the family tree!'
+  puts 'What would you like to do?'
+
+  loop do
+    puts 'Press a to add a family member.'
+    puts 'Press l to list out the family members.'
+    puts 'Press m to add who someone is married to.'
+    puts 'Press s to see who someone is married to.'
+    puts 'Press e to exit.'
+    choice = gets.chomp
+
+    case choice
+    when 'a'
+      add_person
+    when 'l'
+      list
+    when 'm'
+      add_marriage
+    when 's'
+      show_marriage
+    when 'e'
+      exit
+    end
+  end
+end
+
+def add_person
+  puts 'What is the name of the family member?'
+  name = gets.chomp
+  Person.create(:name => name)
+  puts name + " was added to the family tree.\n\n"
+end
+
+def add_marriage
+  list
+  puts 'What is the number of the first spouse?'
+  spouse1 = Person.find(gets.chomp)
+  puts 'What is the number of the second spouse?'
+  spouse2 = Person.find(gets.chomp)
+  spouse1.update(:spouse_id => spouse2.id)
+  puts spouse1.name + " is now married to " + spouse2.name + "."
+end
+
+def list
+  puts 'Here are all your relatives:'
+  people = Person.all
+  people.each do |person|
+    puts person.id.to_s + " " + person.name
+  end
+  puts "\n"
+end
+
+def show_marriage
+  list
+  puts "Enter the number of the relative and I'll show you who they're married to."
+  person = Person.find(gets.chomp)
+  spouse = Person.find(person.spouse_id)
+  puts person.name + " is married to " + spouse.name + "."
+end
+
+menu
